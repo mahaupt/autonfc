00163816c9b2ea71c138828b9934d583455a56f8
mahaupt <subcortexx@gmail.com>
2021-12-30 23:29:25 +0000

    fix setup

--- setup.sh
+++ setup.sh
@@ -5,9 +5,7 @@ touch "/etc/modprobe.d/ftdi.conf"
 echo "blacklist ftdi_sio" >> "/etc/modprobe.d/ftdi.conf"
 echo "blacklist usbserial" >> "/etc/modprobe.d/ftdi.conf"
 
-groupadd usb_access
 usermod -a -G dialout marcel
-usermod -a -G usb_access marcel
 usermod -a -G video marcel
 
 apt install python3 python3-pip libgl1 libzbar0
@@ -15,15 +13,12 @@ pip install -r requirements.txt
 
 cat <<EOT >> /root/.bashrc
 if [ \$(tty) == /dev/tty1 ]; then
-  python3 $(pwd)/main.py
+  cd $(pwd)
+  python3 main.py
 fi
 EOT
 
 # TODO
-# edit /etc/inittab
-# scroll down to
-# 1:2345:respawn:/sbin/getty 115200 tty1
-# change to
-# #1:2345:respawn:/sbin/getty 115200 tty1
-# add line
-# 1:2345:respawn:/bin/login -f root tty1 </dev/tty1 >/dev/tty1 2>&1
+# copy contents of "/lib/systemd/system/getty@.service"
+# launch "systemctl edit getty@tty1.service"
+# Change the ExecStart=- line to "/sbin/agetty -a $DESIRED_USERNAME --noclear %I $TERM"
